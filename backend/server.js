c279b4e32ff0303c7a6170bb1738ed7cd5f7451f
prachitripathi26 <prachitripathi014@gmail.com>
2026-05-24 10:57:49 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔹 MongoDB Connect
+mongoose.connect("mongodb+srv://prachitripathi:prachi123@cluster0.44vxpaq.mongodb.net/ecommerce")
+.then(() => console.log("MongoDB Connected ✅"))
+.catch((err) => console.log(err));
+
+// 🔹 Schema
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  image: String,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+// 🔹 Add Product API
+app.post("/add-product", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.json({ message: "Product Added ✅" });
+});
+
+// 🔹 Get Products API
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// 🔹 Server Start
+app.listen(5000, () => {
+  console.log("Server running on port 5000 🚀");
+
+
+});
